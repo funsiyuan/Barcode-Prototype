0a0a619708d70adcfd359e2067f3affe1f2db1fd
FunSiyuan <47549414+funsiyuan@users.noreply.github.com>
2022-05-17 12:07:21 +0900

    just read onece

--- PRESUS-Barcode-Prototype/ViewController.swift
+++ PRESUS-Barcode-Prototype/ViewController.swift
@@ -15,11 +15,16 @@ final class ViewController: UIViewController {
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label3: UILabel!
     
+    private var currentBarcode: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCamera()
+        
+        label1.text = "バーコードスキャン結果:"
     }
     
+    // MARK: - Setup.
     private func setupCamera() {
         let captureSession = AVCaptureSession()
         captureSession.sessionPreset = .photo
@@ -47,19 +52,40 @@ extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
                 
         let barcodeRequest = VNDetectBarcodesRequest { request, error in
+            
+            // MARK: - Result is here!!
             guard let result = request.results as? [VNBarcodeObservation] else { return }
+            if result.isEmpty { return }
+            
+            if result.first?.payloadStringValue?.debugDescription == self.currentBarcode { return }
+            
+            self.currentBarcode = result.first?.payloadStringValue?.debugDescription ?? ""
+            
+            let generator = UIImpactFeedbackGenerator(style: .heavy)
+            generator.prepare()
+            generator.impactOccurred()
             
             DispatchQueue.main.sync {
-                self.label1.text = "バーコードスキャン結果:"
+                self.appleyBackgroundAnimation()
                 self.label2.text = result.first?.payloadStringValue?.debugDescription
                 self.label3.text = result.first?.symbology.rawValue
             }
             
         }
-        barcodeRequest.symbologies = [.qr, .gs1DataBar, .gs1DataBarLimited, .gs1DataBarExpanded, .code128, .ean13]
+        barcodeRequest.symbologies = [.ean13, .code128, .gs1DataBarExpanded, .gs1DataBarLimited, .gs1DataBar, .dataMatrix, .aztec, .codabar, .code39, .ean13, .code39FullASCII, .code93, .code93i, .microQR, .microPDF417]
         
         try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([barcodeRequest])
         
+    }
+    
+    private func appleyBackgroundAnimation() {
+        self.view.backgroundColor = UIColor(red: 107/255, green: 152/255, blue: 220/255, alpha: 1)
+        self.label2.backgroundColor = .green
+        
+        Timer.scheduledTimer(withTimeInterval: 0.3, repeats: false) { _ in
+            self.label2.backgroundColor = .systemBackground
+            self.view.backgroundColor = .systemBackground
+        }
     }
 }
 
